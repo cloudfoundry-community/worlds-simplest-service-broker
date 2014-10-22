06eade68ee5450cd4cbb706e596cdaa3ae4bcea4
Dr Nic Williams <drnicwilliams@gmail.com>
2014-10-22 13:55:41 -0700

    simple scaffold for web app with negroni

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/codegangsta/negroni"
+)
+
+func init() {
+	log.SetFlags(log.Ltime | log.Lshortfile)
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(w, "Hello World!")
+	})
+
+	n := negroni.Classic()
+
+	// Handler goes last
+	n.UseHandler(mux)
+
+	// Serve
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	n.Run(":" + port)
 }
